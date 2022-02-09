d140e146395261e0b2f49c3638ea3e83dfd603de
Kevin Li <kevinli4321@gmail.com>
2022-02-09 22:09:36 +0000

    Home page route

--- hw2/app.js
+++ hw2/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/ttt', function (req, res) {
-    res.send('GET request to this page');
+    res.sendFile(path.join(__dirname, 'routes/index.html'));
 });
 
 app.listen(port, () => {

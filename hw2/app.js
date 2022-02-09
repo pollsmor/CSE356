c470fe7270f3b7c2c411f3ba1a88e82a675d1ed0
Kevin Li <kevinli4321@gmail.com>
2022-02-09 22:50:12 +0000

    Step 2

--- hw2/app.js
+++ hw2/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ttt', function (req, res) {
     res.sendFile(path.join(__dirname, 'routes/index.html'));
 });
 
+app.post('/', function(req, res) {
+    let name = req.body.name;
+    let date = new Date().toISOString().slice(0, 10);
+    res.send("Hello " + name + ", " + date);
+});
+
 app.listen(port, () => {
     console.log('Warmup project 1 listening on port ' + port);
 });
-

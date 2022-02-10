64d6c7aa51f2b9e420048ca563ca11e8e95ff570
Kevin Li <kevinli4321@gmail.com>
2022-02-10 01:38:55 +0000

    I *think* part 3 is done?

--- hw2/app.js
+++ hw2/app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ttt', function (req, res) {
@@ -15,9 +16,43 @@ app.get('/ttt', function (req, res) {
 app.post('/', function(req, res) {
     let name = req.body.name;
     let date = new Date().toISOString().slice(0, 10);
-    res.send("Hello " + name + ", " + date);
+    res.send('Hello ' + name + ', ' + date);
+});
+
+app.post('/ttt/play', function (req, res) {
+    req.body.winner = getWinner(req.body.grid);
+    res.json(req.body);
 });
 
 app.listen(port, () => {
     console.log('Warmup project 1 listening on port ' + port);
 });
+
+// =======================================================
+function getWinner(board) {
+    if (board[0] == board[1] && board[1] == board[2])
+        return board[0] == 'X' || board[0] == 'O' ? board[0] : ' '; // First row
+
+    else if (board[3] == board[4] && board[4] == board[5])
+        return board[3] == 'X' || board[3] == 'O' ? board[3] : ' '; // Second row
+
+    else if (board[6] == board[7] && board[7] == board[8])
+        return board[6] == 'X' || board[6] == 'O' ? board[6] : ' '; // Third row
+
+    else if (board[0] == board[3] && board[3] == board[6])
+        return board[0] == 'X' || board[0] == 'O' ? board[0] : ' '; // First col
+
+    else if (board[1] == board[4] && board[4] == board[7])
+        return board[1] == 'X' || board[1] == 'O' ? board[1] : ' '; // Second col
+
+    else if (board[2] == board[5] && board[5] == board[8])
+        return board[2] == 'X' || board[2] == 'O' ? board[2] : ' '; // Third col
+
+    else if (board[0] == board[4] && board[4] == board[8])
+        return board[0] == 'X' || board[0] == 'O' ? board[0] : ' '; // Diagonal to the right
+
+    else if (board[2] == board[4] && board[4] == board[6])
+        return board[2] == 'X' || board[2] == 'O' ? board[2] : ' '; // Diagonal to the left
+
+    else return ' ';
+}

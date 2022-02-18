c0af1a53c383c4eac352ab78d81438781b522c39
Kevin Li <kevinli4321@gmail.com>
2022-02-18 17:57:56 +0000

    Clone warmup project 1

---
+++ warmup_proj_2/app.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const pug = require('pug');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.get('/ttt/', function (req, res) {
+    let locals = {
+        pageTitle: 'Warmup Project 1',
+    }
+
+    res.render('form', locals);
+});
+
+app.post('/ttt/', function(req, res) {
+    let locals = {
+        pageTitle: 'Tic-Tac-Toe!',
+        name: req.body.name,
+        date: new Date().toISOString().slice(0, 10)
+    };
+
+    res.render('tictactoe', locals);
+});
+
+app.post('/ttt/play', function (req, res) {
+    let grid = req.body.grid;
+    req.body.winner = getWinner(grid);
+
+    // Make move
+    for (var i = 0; i < 8; i++) {
+        if (grid[i] == ' ') { // Free spot found
+            let randIdx = getRandomInt(9);
+            while (grid[randIdx] != ' ') {
+                randIdx = getRandomInt(9);
+            }
+
+            grid[randIdx] = 'O';
+            req.body.grid = grid;
+            break;
+        }
+    }
+
+    if (req.body.winner == ' ')
+        req.body.winner = getWinner(grid);
+        
+    res.json(req.body);
+});
+
+app.listen(port, () => {
+    console.log('Warmup project 1 listening on port ' + port);
+});
+
+// =======================================================
+function getWinner(board) {
+    if (board[0] == board[1] && board[1] == board[2])
+        return board[0] == 'X' || board[0] == 'O' ? board[0] : ' '; // First row
+
+    else if (board[3] == board[4] && board[4] == board[5])
+        return board[3] == 'X' || board[3] == 'O' ? board[3] : ' '; // Second row
+
+    else if (board[6] == board[7] && board[7] == board[8])
+        return board[6] == 'X' || board[6] == 'O' ? board[6] : ' '; // Third row
+
+    else if (board[0] == board[3] && board[3] == board[6])
+        return board[0] == 'X' || board[0] == 'O' ? board[0] : ' '; // First col
+
+    else if (board[1] == board[4] && board[4] == board[7])
+        return board[1] == 'X' || board[1] == 'O' ? board[1] : ' '; // Second col
+
+    else if (board[2] == board[5] && board[5] == board[8])
+        return board[2] == 'X' || board[2] == 'O' ? board[2] : ' '; // Third col
+
+    else if (board[0] == board[4] && board[4] == board[8])
+        return board[0] == 'X' || board[0] == 'O' ? board[0] : ' '; // Diagonal to the right
+
+    else if (board[2] == board[4] && board[4] == board[6])
+        return board[2] == 'X' || board[2] == 'O' ? board[2] : ' '; // Diagonal to the left
+
+    else return ' ';
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
---
+++ warmup_proj_2/public/js/script.js
@@ -0,0 +1,43 @@
+function makeMove(e) {
+    let str = e.innerText;
+    if (isSpace(str)) { // &nbsp char code
+        let turn = document.getElementById("turn");
+        if (turn.innerText != "X") return;
+        e.innerHTML = "<h1 class=\"symbol\">X</h1>"
+        turn.innerText = "O";
+
+        // Send JSON payload
+        let board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+        let grid = document.getElementById("grid").children;
+        for (var i = 0; i < 9; i++) {
+            if (grid[i].innerText == "X") board[i] = "X";
+            else if (grid[i].innerText == "O") board[i] = "O";
+            else board[i] = " ";
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "/ttt/play",
+            dataType: "json",
+            data: {
+                grid: board
+            },
+            success: function (data) {
+                // Bot makes (random) move
+                for (var i = 0; i < 9; i++) {
+                    grid[i].innerHTML = "<h1 class=\"symbol\">" + data.grid[i] + "</h1>";
+                }
+
+                document.getElementById("winner").innerText = data.winner;
+                turn.innerText = "X";
+            },
+            error: function () {
+                alert("Can't play any more.");
+            }
+        });
+    }
+}
+
+function isSpace(str) {
+    return str === null || str.match(/^ *$/) !== null;
+}

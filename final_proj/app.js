85300673c8938f795fe17bc97d9c5fe78b1420a7
Kevin Li <kevinli4321@gmail.com>
2022-04-02 21:22:07 +0000

    ShareDB *might* be working?

--- final_proj/app.js
+++ final_proj/app.js
@@ -1,14 +1,26 @@
+const http = require('http');
 const express = require('express');
+const WebSocket = require('ws');
+const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
 const ShareDB = require('sharedb');
 
+// Because Quill doesn't use the default type of ShareDB
+ShareDB.types.register(require('rich-text').type);
+
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server: server });
+
 const backend = new ShareDB();
-
-app.use(express.static('public'));
-
-app.listen(3000, () => {
-    console.log('Final project listening on port 3000');
+const connection = backend.connect();
+wss.on('connection', (ws) => {
+    const stream = new WebSocketJSONStream(ws);
+    backend.listen(stream);
 });
+
+const doc = connection.get('documents', 'firstDocument');
+app.use(express.static('public'));
+server.listen(3000);
 
 // Routes ====================================================================
 
@@ -19,5 +31,11 @@ app.get('/', function (req, res) {
 
 app.get('/connect/:id', function (req, res) {
     let id = req.params.id;
-    res.json({lmao: 'wtf'});
+    doc.fetch(function (err) {
+        if (err) throw err;
+        if (doc.type === null)
+            doc.create([], 'rich-text', null);
+    });
+
+    res.json({test: 'wtf'});
 });

9a0afcc3616fb0250c54575bec828f386d4aea7e
Kevin Li <kevinli4321@gmail.com>
2022-04-03 14:34:57 +0000

    Send ops every 3 seconds only

--- final_proj/app.js
+++ final_proj/app.js
@@ -19,7 +19,15 @@ wss.on('connection', (ws) => {
 });
 
 const doc = connection.get('documents', 'firstDocument');
+const streamHeaders = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+};
+
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 server.listen(3000);
 
 // Routes ====================================================================
@@ -33,9 +41,36 @@ app.get('/connect/:id', function (req, res) {
     let id = req.params.id;
     doc.fetch(function (err) {
         if (err) throw err;
+        // Create document if one does not exist
         if (doc.type === null)
             doc.create([], 'rich-text', null);
     });
 
-    res.json({test: 'wtf'});
+    res.writeHead(200, streamHeaders); // Setup stream
+    // First message
+    res.write(JSON.stringify({data: {content: []} }));
+    res.write('\n');
+
+    doc.on('op', function (ops, source) {
+        res.write(JSON.stringify({data: {content: [ops]} }));
+    });
+
+    res.end();
 });
+
+app.post('/op/:id', function (req, res) {
+    console.log(req.body.content);
+    doc.subscribe(function (err) {
+        if (err) throw err;
+        for (let op of req.body.content)
+            doc.submitOp(op);
+    });
+    
+    res.json();
+})
+
+app.get('/doc/:id', function (req, res) {
+    let id = req.params.id;
+    let snapshot = doc.toSnapshot();
+    console.log(snapshot);
+})
--- final_proj/public/js/script.js
+++ final_proj/public/js/script.js
@@ -1,4 +1,13 @@
 const randString = 'abcdefghijklmnopqrstuvwxyz';
+const ops = []; // Array of oplists
+let opsChanged = false; // If true and timer <= 0, POST changes
+let timer = 3;
+
+// Initialize Quill editor
+const quill = new Quill('#editor', {
+  modules: { toolbar: '#toolbar' },
+  theme: 'snow'
+});
 
 function getRandomId() {
   let randLetter = randString.charAt(Math.floor(Math.random() * 26));
@@ -6,13 +15,32 @@ function getRandomId() {
   return randLetter + Date.now() + randLetter2;
 }
 
-// Initialize Quill editor
-const editor = new Quill('#editor', {
-  modules: { toolbar: '#toolbar' },
-  theme: 'snow'
+// Open connection to server
+const id = getRandomId();
+axios.get(`/connect/${id}`);
+
+// Queue up changes to ops array
+quill.on('text-change', function(delta, oldDelta, source) {
+  ops.push(delta.ops);
+  opsChanged = true;
 });
 
-axios.get(`/connect/${getRandomId()}`)
-  .then(function (res) {
-    console.log(res.data);
-  });
+// Only send ops every 3 seconds
+setInterval(function() {
+  if (timer <= 0 && opsChanged) {
+    axios.post(`/op/${id}`, {
+      'content': ops
+    });
+
+    ops.length = 0;
+    opsChanged = false;
+    timer = 3;
+  }
+
+  timer--; // Decrement timer every second
+}, 1000);
+
+const stream = new EventSource(`/connect/${id}`);
+stream.addEventListener('message', message => {
+  console.log(message);
+})
